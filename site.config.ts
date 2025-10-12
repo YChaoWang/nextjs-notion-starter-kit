bb612ec6ae5df4ec5539c00d0a4a760ec8330877
YChaoCode <yichaowang639@gmail.com>
2025-10-12 23:22:27 +0800

    fix: modify rootNotionPageId

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'David-Wang-1d2fe6fa899280b087fff187979d7b89?pvs=4',
+  rootNotionPageId: '1d2fe6fa899280b087fff187979d7b89',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
