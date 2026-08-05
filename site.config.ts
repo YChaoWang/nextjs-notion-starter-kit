b52d913b094f1a9cc446dd3798dc77db0cec6c64
YChaoCode <yichaowang639@gmail.com>
2026-08-05 23:52:57 +0800

    update domain in site configuration from 'www.davidunlimited.com' to 'www.ichauw.com'

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'David',
-  domain: 'www.davidunlimited.com',
+  domain: 'www.ichauw.com',
   author: 'David',
 
   // open graph metadata (optional)
